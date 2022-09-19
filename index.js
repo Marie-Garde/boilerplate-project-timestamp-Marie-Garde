5e9157d25763a2243bf23b8eb43a152ce54fe75c
Marie Garde <marie.garde-isc.france@ibm.com>
2022-09-19 14:59:42 +0200

    test

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
+const { response } = require('express');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -22,6 +23,27 @@ app.get("/", function (req, res) {
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
+});
+
+let responseObject = {}
+// your first API endpoint... 
+app.get(`/api/:input`, function (req, res) {
+  let input = req.params.input
+
+  if (input.includes('-')) {
+    responseObject['unix'] = new Date(input).getTime()
+    responseObject['utc'] = new Date(input).toUTCString()
+  } else {
+    input = parseInt(input)
+
+    responseObject['unix'] = new Date(input).getTime()
+    responseObject['utc'] = new Date(input).toUTCString()
+  }
+
+  if(!responseObject['unix'] || !responseObject['utc']) {
+    response.json({error: 'Invalid Date'})
+  }
+  res.json(responseObject);
 });
 
 
